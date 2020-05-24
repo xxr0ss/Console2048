2114b63dc34c092a7bc44bd9d3797cd825d82c4e
pjx206 <pjx206@163.com>
2020-05-24 23:07:40 +0800

    now can start game and play

--- main.c
+++ main.c
@@ -5,14 +5,13 @@
 #include <conio.h>
 #include "game.h"
 
-// NOTE: maybe it's better to set "PMATRIX pBoard" as global variable;
-#define SIZE 4
+// NOTE: maybe it's better to set "PMATRIX board" as global variable;
 
-typedef int ** MATRIX;
-typedef MATRIX*  PMATRIX;
-
+typedef int **MATRIX;
+typedef MATRIX *PMATRIX;
 
 // Global data
+int boardSize = 4;
 int currentScore;
 int scoreRecord;
 int restZeros;
@@ -23,22 +22,24 @@ int readRecord();
 int encodeRecord();
 int decodeRecord();
 
+void rotateBoardClockwise(MATRIX board, int times);
+int slideLeft(MATRIX board);
+int slideRight(MATRIX board);
+int slideUp(MATRIX board);
+int slideDown(MATRIX board);
+void addNewValToBoard(MATRIX board);
 
-void rotateBoardClockwise(PMATRIX pBoard, int times);
-int slideLeft(PMATRIX pBoard);
-int slideRight(PMATRIX pBoard);
-int slideUp(PMATRIX pBoard);
-int slideDown(PMATRIX pBoard);
-void addNewValToBoard(PMATRIX pBoard);
-void updateScreen(PMATRIX pBoard);
+void getColor(int val, char *buffer, int length);
+void resetColor();
+void displayBoard(MATRIX Board);
+
 BOOL GameInit(OUT PMATRIX *pBoard, OUT PENV *pOldEnv);
-BOOL StartGame(PMATRIX pBoard);
+BOOL StartGame(MATRIX board);
 
 void lookUpRank();
 
-void rotateBoardClockwise(PMATRIX pBoard, int times)
+void rotateBoardClockwise(MATRIX board, int times)
 {
-    MATRIX board = *pBoard;
     // rotate square matrix, (not only 4 x 4 square)
     if (0 == times || 4 == times)
     {
@@ -47,44 +48,43 @@ void rotateBoardClockwise(PMATRIX pBoard, int times)
     while (times--)
     {
         int step = 0;
-        int *tempArr = (int *)malloc(sizeof(int) * (SIZE - step - 1));
-        while (step < (SIZE / 2))
+        int *tempArr = (int *)malloc(sizeof(int) * (boardSize - step - 1));
+        while (step < (boardSize / 2))
         {
             int i;
             int j = 0;
-            for (i = SIZE - 1 - step; i > step; i--)
-                tempArr[j++] = board[i][SIZE - 1 - step];
+            for (i = boardSize - 1 - step; i > step; i--)
+                tempArr[j++] = board[i][boardSize - 1 - step];
 
-            for (i = SIZE - 1 - step; i > step; i--)
-                board[i][SIZE - 1 - step] = board[step][i];
-            for (i = SIZE - 1 - step; i > step; i--)
-                board[step][i] = board[SIZE - 1 - i][step];
-            for (i = step; i < SIZE - 1 - step; i++)
-                board[i][step] = board[SIZE - 1 - step][i];
+            for (i = boardSize - 1 - step; i > step; i--)
+                board[i][boardSize - 1 - step] = board[step][i];
+            for (i = boardSize - 1 - step; i > step; i--)
+                board[step][i] = board[boardSize - 1 - i][step];
+            for (i = step; i < boardSize - 1 - step; i++)
+                board[i][step] = board[boardSize - 1 - step][i];
 
             j = 0;
-            for (i = step; i < SIZE - 1 - step; i++)
+            for (i = step; i < boardSize - 1 - step; i++)
             {
-                board[SIZE - 1 - step][i] = tempArr[j++];
+                board[boardSize - 1 - step][i] = tempArr[j++];
             }
             step++;
         }
     }
 }
 
-int slideUp(PMATRIX pBoard)
+int slideUp(MATRIX board)
 {
-    MATRIX board = *pBoard;
-    int *columnNonZeroNums = (int *)malloc(sizeof(int) * SIZE);
+    int *columnNonZeroNums = (int *)malloc(sizeof(int) * boardSize);
     int columnNumNotZero;
     restZeros = 0;
     int i;
     int val;
-    for (int row, col = 0; col < SIZE; col++)
+    for (int row, col = 0; col < boardSize; col++)
     {
         row = 0;
         columnNumNotZero = 0;
-        while (row < SIZE)
+        while (row < boardSize)
         {
             if (0 == board[row++][col])
             {
@@ -96,7 +96,7 @@ int slideUp(PMATRIX pBoard)
             // having made sure that nonZeroNums is not empty
             // so the code below can handle all 3 cases:
             // "0, equal, not equal"
-            while (row < SIZE)
+            while (row < boardSize)
             {
                 if (0 == (val = board[row++][col]))
                 {
@@ -118,48 +118,47 @@ int slideUp(PMATRIX pBoard)
             board[i][col] = columnNonZeroNums[i];
         }
 
-        for (; i < SIZE; i++)
+        for (; i < boardSize; i++)
         {
             board[i][col] = 0;
         }
 
-        restZeros += SIZE - columnNumNotZero;
+        restZeros += boardSize - columnNumNotZero;
     }
     free(columnNonZeroNums);
     return restZeros;
 }
 
-int slideDown(PMATRIX pBoard)
+int slideDown(MATRIX board)
 {
-    rotateBoardClockwise(pBoard, 2);
-    slideUp(pBoard);
-    rotateBoardClockwise(pBoard, 2);
+    rotateBoardClockwise(board, 2);
+    slideUp(board);
+    rotateBoardClockwise(board, 2);
     return restZeros;
 }
 
-int slideLeft(PMATRIX pBoard)
+int slideLeft(MATRIX board)
 {
-    rotateBoardClockwise(pBoard, 1);
-    slideUp(pBoard);
-    rotateBoardClockwise(pBoard, 3);
+    rotateBoardClockwise(board, 1);
+    slideUp(board);
+    rotateBoardClockwise(board, 3);
     return restZeros;
 }
 
-int slideRight(PMATRIX pBoard)
+int slideRight(MATRIX board)
 {
-    rotateBoardClockwise(pBoard, 3);
-    slideUp(pBoard);
-    rotateBoardClockwise(pBoard, 1);
+    rotateBoardClockwise(board, 3);
+    slideUp(board);
+    rotateBoardClockwise(board, 1);
     return restZeros;
 }
 
-void addNewValToBoard(PMATRIX pBoard)
+void addNewValToBoard(MATRIX board)
 {
-    MATRIX board = *pBoard;
     int pos = rand() % restZeros;
-    for (int i = 0; i < SIZE; i++)
+    for (int i = 0; i < boardSize; i++)
     {
-        for (int j = 0; j < SIZE; j++)
+        for (int j = 0; j < boardSize; j++)
         {
             if (!board[i][j])
             {
@@ -179,6 +178,145 @@ void addNewValToBoard(PMATRIX pBoard)
     }
 }
 
+void displayBoard(MATRIX board)
+{
+    system("cls");
+
+    // DEBUG
+    for (int i = 0; i < boardSize; i++)
+    {
+        for (int j = 0; j < boardSize; j++)
+        {
+            printf("%2d ", board[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf(" - 2048 -\n");
+    printf("Highest score: %-d pts\nCurrent Score: %-d pts\n\n",
+           scoreRecord, currentScore);
+    char color[32];
+    int row, col;
+    for (row = 0; row < boardSize; row++)
+    {
+        for (col = 0; col < boardSize; col++)
+        {
+            getColor(board[row][col], color, 32);
+            printf("%s", color);
+            printf("%7s", "");
+            resetColor();
+        }
+        printf("\n");
+        for (col = 0; col < boardSize; col++)
+        {
+            getColor(board[row][col], color, 32);
+            printf("%s", color);
+            if (board[row][col])
+            {
+                char valStr[8];
+                sprintf_s(valStr, 8, "%d", board[row][col]);
+                int t = 7 - strlen(valStr);
+                printf("%*s%s%*s", t - t / 2, "", valStr, t / 2, "");
+            }
+            else
+            {
+                printf("       ");
+            }
+            resetColor();
+        }
+        printf("\n");
+        for (col = 0; col < boardSize; col++)
+        {
+            getColor(board[row][col], color, 32);
+            printf("%s", color);
+            printf("%7s", "");
+            resetColor();
+        }
+        printf("\n");
+    }
+    printf("\nControl: WSAD or ←,↑,→,↓ to slide blocks");
+    printf("\n         Enter Q to return to home menu");
+}
+
+void getColor(int val, char *buffer, int length)
+{
+    UINT8 color = 0;
+    UINT8 colorPairs[] = {255, 8, 255, 1, 255, 2, 255, 3, 255, 4, 255, 5,
+                          255, 6, 255, 7, 9, 0, 10, 0, 11, 0, 12, 0, 13,
+                          0, 14, 0, 255, 0, 255, 0}; // (f color, b color) pairs
+    if (val)
+    {
+        color = 0;
+        while ((val & 1) ^ 1)
+        {
+            val >>= 1;
+            color++;
+        }
+    }
+    sprintf_s(buffer, length, "\033[38;5;%d;48;5;%dm",
+              colorPairs[color], colorPairs[color + 1]);
+}
+
+void resetColor()
+{
+    printf("\033[m");
+}
+
+BOOL StartGame(MATRIX board)
+{
+
+    displayBoard(board);
+    BOOL willExit = FALSE;
+    int key, key_extra;
+    while (!willExit)
+    {
+        key = _getch();
+        switch (key)
+        {
+        case 'w':
+        case 'W':
+            slideUp(board);
+            break;
+        case 's':
+        case 'S':
+            slideDown(board);
+            break;
+        case 'A':
+        case 'a':
+            slideLeft(board);
+            break;
+        case 'D':
+        case 'd':
+            slideRight(board);
+            break;
+        case 0xE0:
+            switch (key_extra = _getch())
+            {
+            case 72:
+                slideUp(board);
+                break;
+            case 80:
+                slideDown(board);
+                break;
+            case 75:
+                slideLeft(board);
+                break;
+            case 77:
+                slideRight(board);
+                break;
+            default:
+                break;
+            }
+        default:
+            break;
+        }
+        displayBoard(board);
+        addNewValToBoard(board);
+        displayBoard(board);
+    }
+    return TRUE;
+}
+
 int selectionIn(const char **options, int numOfOptions)
 {
     int selection = 0;
@@ -190,10 +328,10 @@ int selectionIn(const char **options, int numOfOptions)
         system("cls");
         for (int i = 0; i < numOfOptions; i++)
         {
-            printf("\n");
-            printf("\t%s %s", i == selection ? prefix : " ", options[i]);
+            printf("\n\n");
+            printf("\t%s %s\n", i == selection ? prefix : " ", options[i]);
         }
-        key = getch();
+        key = _getch();
         switch (key)
         {
         case 'w':
@@ -206,7 +344,7 @@ int selectionIn(const char **options, int numOfOptions)
             continue;
         case 0xE0:
             // ↑, ↓ keys generates a key and a key_extra
-            switch (key_extra = getch())
+            switch (key_extra = _getch())
             {
             case 72:
                 selection = (numOfOptions + selection - 1) % numOfOptions;
@@ -225,66 +363,14 @@ int selectionIn(const char **options, int numOfOptions)
     return selection;
 }
 
-BOOL StartGame(PMATRIX pBoard)
-{
-    BOOL willExit = FALSE;
-    int key, key_extra;
-    while (!willExit)
-    {
-        key = getch();
-        switch (key)
-        {
-        case 'w':
-        case 'W':
-            slideUp(pBoard);
-            break;
-        case 's':
-        case 'S':
-            slideDown(pBoard);
-            break;
-        case 'A':
-        case 'a':
-            slideLeft(pBoard);
-            break;
-        case 'D':
-        case 'd':
-            slideRight(pBoard);
-            break;
-        case 0xE0:
-            switch (key_extra = getch())
-            {
-            case 72:
-                slideUp(pBoard);
-                break;
-            case 80:
-                slideDown(pBoard);
-                break;
-            case 75:
-                slideLeft(pBoard);
-                break;
-            case 77:
-                slideRight(pBoard);
-                break;
-            default:
-                break;
-            }
-        default:
-            break;
-        }
-        updateScreen(pBoard);
-        addNewValToBoard(pBoard);
-        updateScreen(pBoard);
-    }
-}
-
-BOOL mainMenu(PMATRIX pBoard)
+BOOL mainMenu(MATRIX board)
 {
     BOOL willExit = FALSE;
     const char *options[] = {"开始游戏", "查看排名", "退出游戏"};
     switch (selectionIn(options, 3))
     {
     case 0:
-        willExit = StartGame(pBoard);
+        willExit = StartGame(board);
         // saveRecord();
         break;
     case 1:
@@ -298,18 +384,18 @@ BOOL mainMenu(PMATRIX pBoard)
 
 BOOL GameInit(OUT PMATRIX *ppBoard, OUT PENV *pOldEnv)
 {
-    
-    MATRIX board = (MATRIX)malloc(sizeof(int *) * SIZE);
-    for (int i = 0; i < SIZE; i++)
+
+    MATRIX board = (MATRIX)malloc(sizeof(int *) * boardSize);
+    for (int i = 0; i < boardSize; i++)
     {
-        board[i] = (int *)malloc(sizeof(int) * SIZE);
-        memset(board[i], 0, sizeof(int) * SIZE);
+        board[i] = (int *)malloc(sizeof(int) * boardSize);
+        memset(board[i], 0, sizeof(int) * boardSize);
     }
     *ppBoard = &board;
 
     srand((unsigned)time(NULL));
     restZeros = 16;
-    addNewValToBoard(&board);
+    addNewValToBoard(board);
 
     // hide cursor after save origin settings
     PENV oldEnv = *pOldEnv;
@@ -322,6 +408,16 @@ BOOL GameInit(OUT PMATRIX *ppBoard, OUT PENV *pOldEnv)
     // Read record from file, and maybe decrypt it before read record
     scoreRecord = 100; // assuming the highest score record
     currentScore = 0;
+
+    // DEBUG
+    for (int i = 0; i < boardSize; i++)
+    {
+        for (int j = 0; j < boardSize; j++)
+        {
+            printf("%2d ", board[i][j]);
+        }
+        printf("\n");
+    }
 
     return SetConsoleTitleW(L"进击的2048");
 }
@@ -338,11 +434,12 @@ int main()
     PMATRIX pBoard;
     if (!GameInit(&pBoard, &oldEnv))
         return EXIT_FAILURE;
+    MATRIX board = *pBoard;
 
     BOOL willExit;
     do
     {
-        willExit = mainMenu(pBoard);
+        willExit = mainMenu(board);
     } while (!willExit);
 
     return ConsoleRecovery(oldEnv);
